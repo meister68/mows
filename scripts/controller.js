bb3b0d14166ea9723a0146719f92d80c206469de
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-08-28 18:08:28 +0800

    can publish, subscribe. No broker table data

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,93 @@
-// basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+$(document).ready(function () {
 
-client.on("connect", function(){
-    console.log("Successfully connected");
-})
+  var broker = $("#brokerAddress").val();
+  var payload = $("#payload");
+  var topic = $("#topic");
+  var topicSubscribe =  $("#topicSubscribe");
+  var timestamp = new Date($.now());
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
 
-client.publish("mqtt/demo", "hello world!")
+  $("#btnConnect").click(function () {
+    // basic functionalities
+    client = mqtt.connect(broker);
+  
+    client.on("connect", function () {
+      $("#status").val("Connected");
+    });
+
+  });
+
+  $("#btnDisconnect").click(function(){
+    client.end();
+    $("#status").val("Disconnected");
+  })
+
+  $("#btnPublish").click(function () {
+    client.publish(topic.val(), payload.val());
+    $("#publishTopics tbody").append("<tr> <td>" + topic.val() + "</td>" +
+      "<td>" + payload.val() + "</td>" +
+      "<td>" +timestamp.toUTCString()+ "</td>" +
+      "</tr>"
+    );
+
+
+  });
+
+  $("#btnSubscribe").click(function () {
+    client.subscribe(topicSubscribe.val());
+    
+    client.on("message", function (topic, payload) {
+      console.log([topic, payload].join(": "));
+    });
+
+    $("#subscribeTopics tbody").append("<tr> <td>" + topicSubscribe.val() + "</td>" +
+    "<td>" +timestamp.toUTCString()+ "</td>" +
+    "</tr>"
+  );
+
+  });
+
+  $("#btnUnSubscribe").click(function(){
+    client.unsubscribe(topic.val());
+
+  })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
